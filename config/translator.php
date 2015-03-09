eb22295abcc4cb0eaf1dfbcad166d5c5004264c6
Vincent Klaiber <vincentklaiber@gmail.com>
2015-03-09 09:48:25 +0100

    Fix PSR-2 error.

--- config/translator.php
+++ config/translator.php
@@ -2,41 +2,41 @@
 
 return [
 
-	/*
-	|--------------------------------------------------------------------------
-	| Locale Eloquent Model
-	|--------------------------------------------------------------------------
-	|
-	| This is the Eloquent model that handles what languages you support within
-	| your project. Please provide the full namespaced path.
-	|
-	*/
+    /*
+    |--------------------------------------------------------------------------
+    | Locale Eloquent Model
+    |--------------------------------------------------------------------------
+    |
+    | This is the Eloquent model that handles what languages you support within
+    | your project. Please provide the full namespaced path.
+    |
+    */
 
-	'locale' => 'Acme\Locales\Locale',
+    'locale' => 'Acme\Locales\Locale',
 
-	/*
-	|--------------------------------------------------------------------------
-	| Locale Identifier Column
-	|--------------------------------------------------------------------------
-	|
-	| Specify the column key that you want to compare with when fetching
-	| translations trough the App::getLocale() method.
-	|
-	*/
+    /*
+    |--------------------------------------------------------------------------
+    | Locale Identifier Column
+    |--------------------------------------------------------------------------
+    |
+    | Specify the column key that you want to compare with when fetching
+    | translations trough the App::getLocale() method.
+    |
+    */
 
-	'column' => 'language',
+    'column' => 'language',
 
-	/*
-	|--------------------------------------------------------------------------
-	| Fallback Support
-	|--------------------------------------------------------------------------
-	|
-	| Set this to true if you want to fetch the default translation if the
-	| current locale doesn't have any translated attributes yet. The default
-	| fallback is fetched from app/config/app.php
-	|
-	*/
+    /*
+    |--------------------------------------------------------------------------
+    | Fallback Support
+    |--------------------------------------------------------------------------
+    |
+    | Set this to true if you want to fetch the default translation if the
+    | current locale doesn't have any translated attributes yet. The default
+    | fallback is fetched from app/config/app.php
+    |
+    */
 
-	'fallback' => false
+    'fallback' => false,
 
 ];
