64a0df836697ec15cb777438d143334c1b905dbe
Vincent Klaiber <vincentklaiber@gmail.com>
2015-03-09 09:50:35 +0100

    Add PSR-2 syntax on migrations.

--- database/migrations/2015_01_01_000000_create_locales_table.php
+++ database/migrations/2015_01_01_000000_create_locales_table.php
@@ -3,31 +3,30 @@
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateLocalesTable extends Migration {
+class CreateLocalesTable extends Migration
+{
 
-	/**
-	 * Run the migrations.
-	 *
-	 * @return void
-	 */
-	public function up()
-	{
-		Schema::create('locales', function(Blueprint $table)
-		{
-			$table->increments('id');
-			$table->string('language', 2);
-			$table->timestamps();
-		});
-	}
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('locales', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('language', 2);
+            $table->timestamps();
+        });
+    }
 
-	/**
-	 * Reverse the migrations.
-	 *
-	 * @return void
-	 */
-	public function down()
-	{
-		Schema::drop('locales');
-	}
-
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('locales');
+    }
 }
