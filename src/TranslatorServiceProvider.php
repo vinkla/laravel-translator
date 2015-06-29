41395204caf51c73bdb76c0fe5b9239b4ad9be65
Vincent Klaiber <vincentklaiber@gmail.com>
2015-06-29 13:07:53 +0200

    Only publish config if we're using full stack laravel

--- src/TranslatorServiceProvider.php
+++ src/TranslatorServiceProvider.php
@@ -39,7 +39,11 @@ class TranslatorServiceProvider extends ServiceProvider
     protected function setupConfig()
     {
         $source = realpath(__DIR__.'/../config/translator.php');
-        $this->publishes([$source => config_path('translator.php')]);
+        
+        if (class_exists('Illuminate\Foundation\Application', false)) {
+            $this->publishes([$source => config_path('translator.php')]);
+        }
+        
         $this->mergeConfigFrom($source, 'translator');
     }
 
