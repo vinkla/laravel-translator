18eee5cdd2d0fedecf3568bc9a367086b70538f1
Vincent Klaiber <vincentklaiber@gmail.com>
2015-08-04 15:21:51 +0200

    Make locale id a string

--- database/migrations/2015_01_01_000000_create_locales_table.php
+++ database/migrations/2015_01_01_000000_create_locales_table.php
@@ -11,6 +11,7 @@
 
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
 
 /**
  * This is the locales table seeder class.
@@ -27,8 +28,7 @@ class CreateLocalesTable extends Migration
     public function up()
     {
         Schema::create('locales', function (Blueprint $table) {
-            $table->increments('id');
-            $table->string('language', 2);
+            $table->string('id', 2)->index()->primary();
             $table->timestamps();
         });
     }
