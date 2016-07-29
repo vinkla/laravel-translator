874fcdf258398171715a01620e63f87f3ae7ed7c
Vincent Klaiber <vincentklaiber@gmail.com>
2016-07-29 18:50:02 +0200

    Cleanup tests

--- tests/TranslatorTest.php
+++ tests/TranslatorTest.php
@@ -26,18 +26,21 @@ class TranslatorTest extends AbstractTestCase
     public function testInterface()
     {
         $article = new ReflectionClass(Article::class);
+
         $this->assertTrue($article->implementsInterface(IsTranslatable::class));
     }
 
     public function testHasMany()
     {
         $article = Article::first();
+
         $this->assertSame(2, $article->translations()->count());
     }
 
     public function testTranslate()
     {
         $article = Article::first();
+
         $this->assertSame($article->translate('en')->title, 'Use the force Harry');
         $this->assertSame($article->translate('sv')->title, 'Använd kraften Harry');
     }
@@ -45,8 +48,10 @@ class TranslatorTest extends AbstractTestCase
     public function testLocale()
     {
         $article = Article::first();
+
         $locale = $this->getProtectedMethod($article, 'getLocale');
         $this->assertSame('sv', $locale);
+
         $fallback = $this->getProtectedMethod($article, 'getFallback');
         $this->assertSame('en', $fallback);
     }
@@ -54,23 +59,29 @@ class TranslatorTest extends AbstractTestCase
     public function testFallback()
     {
         $article = Article::first();
+
         $this->assertSame($article->translate('de')->title, 'Use the force Harry');
         $this->assertSame($article->translate('de', true)->title, 'Use the force Harry');
         $this->assertSame($article->translate('de', false)->title, null);
 
         App::setLocale('sv');
+
         $this->assertSame($article->translate('sv', false)->title, 'Använd kraften Harry');
         $this->assertSame($article->translate('nl', false)->title, null);
         $this->assertSame($article->translate('sv', false)->title, 'Använd kraften Harry');
+
         $this->assertSame(App::getLocale(), 'sv');
     }
 
     public function testSetLocale()
     {
         $article = Article::first();
+
         $this->assertSame($article->title, 'Använd kraften Harry');
         $this->assertSame($article->translate()->title, 'Använd kraften Harry');
+
         App::setLocale('en');
+
         $this->assertSame($article->title, 'Use the force Harry');
         $this->assertSame($article->translate()->title, 'Use the force Harry');
     }
@@ -78,11 +89,15 @@ class TranslatorTest extends AbstractTestCase
     public function testCachedTranslations()
     {
         $article = Article::first();
+
         $translations = ['en' => $article->translate('en'), 'sv' => $article->translate('sv')];
         $cache = $this->getProtectedProperty($article, 'cache');
+
         $this->assertCount(2, $cache);
         $this->assertSame($translations, $cache);
+
         DB::enableQueryLog();
+
         $article->translate('en');
         $this->assertEmpty(DB::getQueryLog());
     }
@@ -90,6 +105,7 @@ class TranslatorTest extends AbstractTestCase
     public function testGetAttributes()
     {
         $article = Article::first();
+
         $this->assertSame($article->translate()->title, 'Använd kraften Harry');
         $this->assertSame($article->title, 'Använd kraften Harry');
     }
@@ -97,9 +113,13 @@ class TranslatorTest extends AbstractTestCase
     public function testSetAttributes()
     {
         App::setLocale('en');
+
         $article = Article::first();
+
         $this->assertSame($article->title, 'Use the force Harry');
+
         $article->title = 'I\'m your father Hagrid';
+
         $this->assertSame($article->title, 'I\'m your father Hagrid');
         $this->assertSame($article->translate()->title, 'I\'m your father Hagrid');
         $this->assertSame($article->translate('sv')->title, 'Använd kraften Harry');
@@ -108,11 +128,16 @@ class TranslatorTest extends AbstractTestCase
     public function testCreate()
     {
         App::setLocale('en');
+
         $article = Article::create(['title' => 'Whoa. This is heavy.', 'thumbnail' => 'http://i.imgur.com/tyfwfEX.jpg']);
+
         $this->seeInDatabase('article_translations', ['title' => 'Whoa. This is heavy.', 'article_id' => $article->id, 'locale' => 'en']);
         $this->seeInDatabase('articles', ['thumbnail' => 'http://i.imgur.com/tyfwfEX.jpg']);
+
         App::setLocale('de');
+
         $article = Article::create(['title' => 'Whoa. Das ist schwer.', 'thumbnail' => 'http://i.imgur.com/tyfwfEX.jpg']);
+
         $this->seeInDatabase('article_translations', ['title' => 'Whoa. Das ist schwer.', 'article_id' => $article->id, 'locale' => 'de']);
         $this->seeInDatabase('articles', ['thumbnail' => 'http://i.imgur.com/tyfwfEX.jpg']);
     }
@@ -120,22 +145,30 @@ class TranslatorTest extends AbstractTestCase
     public function testUpdate()
     {
         App::setLocale('en');
+
         $article = Article::find(1);
         $article->title = 'Whoa. This is heavy.';
         $article->save();
+
         $this->seeInDatabase('article_translations', ['title' => 'Whoa. This is heavy.', 'article_id' => $article->id, 'locale' => 'en']);
+
         App::setLocale('sv');
+
         $article->update(['title' => 'Whoa. Detta är tung.']);
+
         $this->seeInDatabase('article_translations', ['title' => 'Whoa. Detta är tung.', 'article_id' => $article->id, 'locale' => 'sv']);
+
         App::setLocale('de');
+
         $article->update(['title' => 'Whoa. Das ist schwer.']);
+
         $this->seeInDatabase('article_translations', ['title' => 'Whoa. Das ist schwer.', 'article_id' => $article->id, 'locale' => 'de']);
     }
 
     public function testDeleteTranslations()
     {
-        $article = Article::first();
-        $article->translations()->delete();
+        Article::first()->translations()->delete();
+
         $this->assertSame(1, Article::count());
         $this->assertSame(0, ArticleTranslation::count());
     }
@@ -143,6 +176,7 @@ class TranslatorTest extends AbstractTestCase
     public function testDeleteParent()
     {
         Article::first()->delete();
+
         $this->assertSame(0, Article::count());
         $this->assertSame(0, ArticleTranslation::count());
     }
